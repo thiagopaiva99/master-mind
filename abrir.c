d37233d088481e8497b163dd7fdfee1abefec47b
Thiago Galvani <thiagopaiva99@gmail.com>
2017-09-20 15:03:25 -0300

    Commitando o arquivo abrir.c

--- abrir.c
+++ abrir.c
@@ -1,8 +1,16 @@
+//faz a inclusão das funcoes
 #include "functions.h"
 
+// funcao para abrir um arquivo - espera o arquivo para abrir
 void abrir( char tipo [ 3 ] ){
-	if( ( bd = fopen( dataBase, tipo ) )== NULL ){
+
+    // verifica se o banco de dados vaii ser nulo
+    // se for ele encerra a plicação
+    // se nao for segue o jogo
+	if( ( bd = fopen( dataBase, tipo ) ) == NULL ){
 		printf("\nNão foi possivel abrir o arquivo\n");
 		exit(1);
 	}
+
+	// final da função
 }
