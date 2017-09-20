e97f0d23300171723fefded6b9409bbb2ec631f9
Thiago Galvani <thiagopaiva99@gmail.com>
2017-09-20 15:12:28 -0300

    Commitando o arquivo consulta_apelido.c

--- functions.h
+++ functions.h
@@ -5,18 +5,42 @@
 #include <time.h>
 
 //FUNCOES
+
+// funcao de jogar
 void jogar(void);
+
+// funcao de novo jogador
 void novo_jogador(void);
+
+// funcao que chama o jogo
 int jogo( char username[20] );
+
+// funcao que sorteia os chars
 char sort(char *s_correto);
+
+// funcao do perfil
 void perfil(void);
+
+// funcao do ranking
 void ranking(void);
+
+// funcao de como jogar
 void comojogar(void);
+
+// funcao que consulta o apelido
 int consulta_apelido(char *apelido);
+
+// funcao para letras minusculas
 void strtolower(char *to_lower);
+
+// funcao para letras maiusculas
 void strtoupper(char *to_upper);
+
+// funcao para sair
 void sair(void);
+
+// funcao para abri arquivos
 void abrir(char *tipo);
 
-// main menu functions
+// funcao para escolher a opção do menu
 void choise(char op);
